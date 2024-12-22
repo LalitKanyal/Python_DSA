42ad3b82398bf4962057b1bc4de5e7331039aab6
Lalit <mailtolalit21@gmail.com>
2024-12-22 10:35:28 +0530

    update

--- Ch01_Basics_Of_Python/1.1.objects/b_create_object.py
+++
@@ -1,7 +0,0 @@
-# 1.2.2 CREATING AND USING OBJECTS
-
-# Instantiation - process of creating a new instance of a class is called instantiation
-
-# Calling Methods
-
-# member function
--- Ch01_Basics_Of_Python/1.5.IO/a_io.py
+++
@@ -1,8 +0,0 @@
-# print and input functionge
-
-age = input("Enter your age: ")
-if age.isdigit():
-    age = float(age)
-    print(f"You are {age} years old")
-else:
-    print("Enter the age in digits")
