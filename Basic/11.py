b8683ba51f6159355405dfbabf5236e9bed6d5ec
Lalit <mailtolalit21@gmail.com>
2024-12-24 10:03:41 +0530

    function documentation printer

---
+++ Basic/11.py
@@ -0,0 +1,23 @@
+'''
+11. Function Documentation Printer
+
+Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+'''
+
+print(abs.__doc__)
+
+print(sum.__doc__)
+
+print(print.__doc__)
+
+print(len.__doc__)
+
+print(sorted.__doc__)
+
+print(map.__doc__)
+
+print(filter.__doc__)
